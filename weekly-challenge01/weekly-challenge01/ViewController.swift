10e453498a630a14310aaef7921537897facc836
SeongHwan-Seo <seosh5704@gmail.com>
2022-08-23 22:11:49 +0900

    Autolayout tutorial

--- weekly-challenge01/weekly-challenge01/ViewController.swift
+++ weekly-challenge01/weekly-challenge01/ViewController.swift
@@ -12,20 +12,21 @@ import SnapKit
 class ViewController: UIViewController {
     
     //let backButton = UIButton()
-    var backButton: UIButton = {
-       let btn = UIButton()
+    lazy var backButton: UIButton = {
+        let btn = UIButton()
         btn.setImage(UIImage(named: "back"), for: .normal)
         return btn
     }()
-    let welcomeLabel: UILabel = {
-       let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 29, weight: .bold)
-        label.text = "Welcome back! Glad to see you, Again!"
-        
-        label.numberOfLines = 0
+    lazy var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.text = "Welcome back! Glad \nto see you, Again!"
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        label.numberOfLines = 2
         return label
     }()
-    let emailTextField: UITextField = {
+    lazy var emailTextField: UITextField = {
         let textField = UITextField()
         
         textField.layer.cornerRadius = 8
@@ -33,12 +34,14 @@ class ViewController: UIViewController {
         
         textField.placeholder = "Enter your email"
         textField.addLeftPadding()
+        textField.adjustsFontSizeToFitWidth = true
+        
         
         textField.layer.applyShadow(color: UIColor(red: 0, green: 0, blue: 0, alpha: 0.25), alpha: 1, x: 0, y: 4, blur: 10, spread: 0)
         
         return textField
     }()
-    let passwordTextField: UITextField = {
+    lazy var passwordTextField: UITextField = {
         let textField = UITextField()
         
         textField.layer.cornerRadius = 8
@@ -52,8 +55,8 @@ class ViewController: UIViewController {
         return textField
     }()
     
-    let forgotLabel: UILabel = {
-       let label = UILabel()
+    lazy var forgotLabel: UILabel = {
+        let label = UILabel()
         
         label.text = "Forgot Password?"
         label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
@@ -61,8 +64,8 @@ class ViewController: UIViewController {
         return label
     }()
     
-    let loginButton: UIButton = {
-       let button = UIButton()
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
         
         button.setTitle("Login", for: .normal)
         button.backgroundColor = UIColor(red: 131/255, green: 145/255, blue: 161/255, alpha: 1)
@@ -75,41 +78,129 @@ class ViewController: UIViewController {
         return button
     }()
     
-    let dividerLabel: UILabel = {
-       let label = UILabel()
-        
-        label.frame = CGRect(x: 0, y: 0, width: 112, height: 0)
-        return label
+    
+    lazy var withLoginStackView : UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [firstDividerView, withLoginLabel, secondDividerView])
+        stackView.spacing = 12
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.distribution = .equalCentering
+        return stackView
     }()
     
-    let dividerLabel_2: UILabel = {
-       let label = UILabel()
-        
-        label.frame = CGRect(x: 0, y: 0, width: 112, height: 0)
-        return label
+    lazy var firstDividerView: UIView = {
+        let view = UIView()
+        view.layer.borderWidth = 1
+        view.frame = CGRect(x: 0, y: 0, width: 112, height: 1)
+        view.layer.borderColor = UIColor(red: 0.91, green: 0.925, blue: 0.957, alpha: 1).cgColor
+        return view
     }()
     
-    
-    let stroke: UIView = {
-       let strokeView = UIView()
-        
-        return strokeView
-    }()
-    
-    let withLabel: UILabel = {
-       let label = UILabel()
-        
+    lazy var withLoginLabel: UILabel = {
+        let label = UILabel()
         label.text = "Or Login with"
-        label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
-        label.textColor = UIColor(red: 0.416, green: 0.439, blue: 0.486, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        return label
+    }()
+    
+    lazy var secondDividerView: UIView = {
+        let view = UIView()
+        view.layer.borderWidth = 1
         
+        view.layer.borderColor = UIColor(red: 0.91, green: 0.925, blue: 0.957, alpha: 1).cgColor
+        return view
+    }()
+    
+    lazy var socialStackView : UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [facebookView,googleView,appleView])
+        
+        stackView.spacing = 12
+        
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    lazy var facebookView: UIView = {
+       let view = UIView()
+        view.addSubview(facebookButton)
+        
+        return view
+    }()
+    
+    lazy var facebookButton: UIButton = {
+       let button = UIButton()
+        button.snp.makeConstraints{
+            $0.height.equalTo(56)
+            $0.width.equalTo(105)
+        }
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 8
+        button.layer.borderColor = UIColor(red: 0.91, green: 0.925, blue: 0.957, alpha: 1).cgColor
+        button.setImage(UIImage(named: "facebook_ic"), for: .normal)
+        return button
+    }()
+    
+    lazy var googleView: UIView = {
+       let view = UIView()
+        view.addSubview(googleButton)
+        
+        return view
+    }()
+    
+    lazy var googleButton: UIButton = {
+       let button = UIButton()
+        button.snp.makeConstraints{
+            $0.height.equalTo(56)
+            $0.width.equalTo(105)
+        }
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 8
+        button.layer.borderColor = UIColor(red: 0.91, green: 0.925, blue: 0.957, alpha: 1).cgColor
+        button.setImage(UIImage(named: "google_ic"), for: .normal)
+        return button
+    }()
+    
+    lazy var appleView: UIView = {
+       let view = UIView()
+        view.addSubview(appleButton)
+        
+        return view
+    }()
+    
+    lazy var appleButton: UIButton = {
+       let button = UIButton()
+        button.snp.makeConstraints{
+            $0.height.equalTo(56)
+            $0.width.equalTo(105)
+        }
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 8
+        button.layer.borderColor = UIColor(red: 0.91, green: 0.925, blue: 0.957, alpha: 1).cgColor
+        button.setImage(UIImage(named: "apple_ic"), for: .normal)
+        return button
+    }()
+    
+    lazy var registerLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Don’t have an account? Register Now"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        
+        let attrs1 = [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15, weight: .medium), NSAttributedString.Key.foregroundColor : UIColor.black]
 
-        
+        let attrs2 = [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15, weight: .semibold), NSAttributedString.Key.foregroundColor : UIColor.systemMint]
+
+           let attributedString1 = NSMutableAttributedString(string:"Don’t have an account? ", attributes:attrs1)
+
+           let attributedString2 = NSMutableAttributedString(string:"Register Now", attributes:attrs2)
+
+           attributedString1.append(attributedString2)
+           label.attributedText = attributedString1
         return label
     }()
     
     
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -124,18 +215,9 @@ class ViewController: UIViewController {
         self.view.addSubview(passwordTextField)
         self.view.addSubview(forgotLabel)
         self.view.addSubview(loginButton)
-        
-        stroke.bounds = dividerLabel.bounds.insetBy(dx: -0.5, dy: -0.5)
-        stroke.center = dividerLabel.center
-        dividerLabel.addSubview(stroke)
-        dividerLabel.bounds = dividerLabel.bounds.insetBy(dx: -0.5, dy: -0.5)
-
-        stroke.layer.borderWidth = 1
-
-        stroke.layer.borderColor = UIColor(red: 0.91, green: 0.925, blue: 0.957, alpha: 1).cgColor
-        
-        self.view.addSubview(dividerLabel)
-        self.view.addSubview(withLabel)
+        self.view.addSubview(withLoginStackView)
+        self.view.addSubview(socialStackView)
+        self.view.addSubview(registerLabel)
         
         backButton.snp.makeConstraints{
             $0.top.equalToSuperview().offset(56)
@@ -144,9 +226,8 @@ class ViewController: UIViewController {
         
         welcomeLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(22)
+            $0.trailing.equalToSuperview().offset(-73)
             $0.top.equalToSuperview().offset(125)
-            $0.width.equalTo(280)
-            $0.height.equalTo(78)
         }
         
         emailTextField.snp.makeConstraints {
@@ -177,19 +258,27 @@ class ViewController: UIViewController {
             $0.height.equalTo(56)
         }
         
-        dividerLabel.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(22)
-            $0.trailing.equalTo(withLabel.snp.leading).offset(-22)
-            $0.top.equalTo(loginButton.snp.bottom).offset(44)
+        withLoginStackView.snp.makeConstraints{
+            $0.top.equalTo(loginButton.snp.bottom).offset(35)
+            $0.leading.equalTo(loginButton.snp.leading)
         }
         
-        withLabel.snp.makeConstraints{
-            $0.leading.equalTo(stroke.snp.trailing).offset(12)
-            $0.top.equalTo(loginButton.snp.bottom).offset(35)
+        socialStackView.snp.makeConstraints{
+            $0.leading.equalToSuperview().offset(22)
+            $0.trailing.equalTo(loginButton.snp.trailing)
+            $0.top.equalTo(withLoginStackView.snp.bottom).offset(22)
         }
+        
+        registerLabel.snp.makeConstraints{
+            $0.bottom.equalToSuperview().offset(-26)
+            $0.centerX.equalToSuperview()
+        }
+        
+        
+        
     }
-
-
+    
+    
 }
 
 #if DEBUG
